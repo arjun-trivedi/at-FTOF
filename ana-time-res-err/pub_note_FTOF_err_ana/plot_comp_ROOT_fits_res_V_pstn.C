f37409d5db644e55e32e62cf8771480676679477
Arjun <arjuntvd@gmail.com>
2015-08-05 08:43:56 -0400

    Updated 'ana-time-res-err/pub_note_FTOF_err_ana/plot_comp_ROOT_fits_res_V_pstn.C' to include MLE-actual results and used it to generate an updated 'ana-time-res-err/pub_note_FTOF_err_ana/res_vs_pstn.pdf' that in turn updated that error analysis note.
    Note, in addition, I added .gitignore.

--- ana-time-res-err/pub_note_FTOF_err_ana/plot_comp_ROOT_fits_res_V_pstn.C
+++ ana-time-res-err/pub_note_FTOF_err_ana/plot_comp_ROOT_fits_res_V_pstn.C
@@ -1,8 +1,8 @@
 void plot_comp_ROOT_fits_res_V_pstn(bool plot_N_V_pstn=true){
   char* DATADIR=gSystem->ExpandPathName("$FTOF_DATADIR/analyzed/stat_err_ana/40-day-run-2-day-cycles");
   
-  const int NMTHD=2;
-  enum {CSQWW,CSQ};
+  const int NMTHD=3;
+  enum {CSQWW,CSQ,MLE};
   
   TFile* fin[NMTHD];
   //! [08-02-15] 
@@ -14,36 +14,46 @@ void plot_comp_ROOT_fits_res_V_pstn(bool plot_N_V_pstn=true){
   char* fname="July-06-07-2014-cycle1-1st-article_321654.root";
   fin[CSQWW]=TFile::Open(TString::Format("%s/MLE/%s",DATADIR,fname));
   fin[CSQ]=  TFile::Open(TString::Format("%s/CSQ/%s",DATADIR,fname));
+  fin[MLE]=  TFile::Open(TString::Format("%s/MLE-actual/%s",DATADIR,fname));
   
 
   TH1F* h[NMTHD];
   h[CSQWW]=(TH1F*)fin[CSQWW]->Get("fullbar_tw/resol_2_4_6_cont");
   h[CSQ]=  (TH1F*)fin[CSQ]->Get("fullbar_tw/resol_2_4_6_cont");
+  h[MLE]=  (TH1F*)fin[MLE]->Get("fullbar_tw/resol_2_4_6_cont");
   //! aesthetics
   h[CSQWW]->SetName(TString::Format("%s_CSQWW",h[CSQWW]->GetName()));
   h[CSQ]->SetName(TString::Format("%s_CSQ",h[CSQ]->GetName()));
+  h[MLE]->SetName(TString::Format("%s_MLE",h[MLE]->GetName()));
   h[CSQWW]->SetMarkerColor(kRed);
   h[CSQWW]->SetLineColor(kRed);
   h[CSQ]->SetLineColor(kBlue);
   h[CSQ]->SetMarkerColor(kBlue);
+  h[MLE]->SetLineColor(kBlack);
+  h[MLE]->SetMarkerColor(kBlack);
 
   //! Quadratic fit = pol2 = [2]*x**2 + [1]*x + [0]
   TF1* f[NMTHD];
   f[CSQWW]=new TF1("fCSQWW","pol2"); 
   f[CSQ]=new TF1("fCSQ","pol2");
+  f[MLE]=new TF1("fMLE","pol2");
   //aesthetics
   f[CSQWW]->SetLineColor(kRed);
   f[CSQ]->SetLineColor(kBlue);
+  f[MLE]->SetLineColor(kBlack);
 
   //! Make plot
   TCanvas* c=new TCanvas();
   h[CSQWW]->Draw();
   h[CSQ]->Draw("sames");
+  h[MLE]->Draw("sames");
   gStyle->SetOptStat("ne");
   //! Fit
   gStyle->SetOptFit(1111);
   h[CSQWW]->Fit(f[CSQWW],"","sames"); 
   h[CSQ]->Fit(f[CSQ],"","sames");
+  //! Parabola fit does not work for MLE because of "outlier" effect
+  //h[MLE]->Fit(f[MLE],"","sames");
   c->Update();
   c->SaveAs("${WORKSPACE}/at-FTOF/ana-time-res-err/pub_note_FTOF_err_ana/res_vs_pstn.pdf");
 
@@ -54,6 +64,7 @@ void plot_comp_ROOT_fits_res_V_pstn(bool plot_N_V_pstn=true){
   float xmax=h[CSQWW]->GetXaxis()->GetXmax();
   htmp[CSQWW]=new TH1F("CSQ","CSQ",nbins,xmin,xmax);
   htmp[CSQ]=new TH1F("CSQWW","CSQWW",nbins,xmin,xmax);
+  htmp[MLE]=new TH1F("MLE","MLE",nbins,xmin,xmax);
   for (int ibin=0;ibin<nbins;ibin++){
     float sgma_csqww=h[CSQWW]->GetBinContent(ibin+1);
     float sgma_sgma_csqww=h[CSQWW]->GetBinError(ibin+1);
@@ -65,6 +76,10 @@ void plot_comp_ROOT_fits_res_V_pstn(bool plot_N_V_pstn=true){
     float sgma_sgma_csq=h[CSQ]->GetBinError(ibin+1);
     htmp[CSQ]->SetBinContent(ibin+1,(sgma_sgma_csq/sgma_csq)*100);
     htmp[CSQ]->SetBinError(ibin+1,0);
+    float sgma_mle=h[MLE]->GetBinContent(ibin+1);
+    float sgma_sgma_mle=h[MLE]->GetBinError(ibin+1);
+    htmp[MLE]->SetBinContent(ibin+1,(sgma_sgma_mle/sgma_mle)*100);
+    htmp[MLE]->SetBinError(ibin+1,0);
   }
   htmp[CSQWW]->SetMarkerStyle(kFullCircle);
   htmp[CSQWW]->SetMarkerColor(kRed);
@@ -72,9 +87,13 @@ void plot_comp_ROOT_fits_res_V_pstn(bool plot_N_V_pstn=true){
   htmp[CSQ]->SetMarkerStyle(kFullCircle);
   htmp[CSQ]->SetLineColor(kBlue);
   htmp[CSQ]->SetMarkerColor(kBlue);
+  htmp[MLE]->SetMarkerStyle(kFullCircle);
+  htmp[MLE]->SetLineColor(kBlack);
+  htmp[MLE]->SetMarkerColor(kBlack);
   TCanvas* c1=new TCanvas();
   htmp[CSQWW]->Draw("E1 P");
   htmp[CSQ]->Draw("E1 P sames");
+  htmp[MLE]->Draw("E1 P sames");
   
 
   if (plot_N_V_pstn){
